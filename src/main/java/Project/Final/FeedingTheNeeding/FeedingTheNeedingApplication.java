130535df01c1af4756883a3fc1ea101e238abf74
ShavitMor <102467231+ShavitMor@users.noreply.github.com>
2024-11-25 16:17:27 +0200

    init db

--- src/main/java/Project/Final/FeedingTheNeeding/FeedingTheNeedingApplication.java
+++ src/main/java/Project/Final/FeedingTheNeeding/FeedingTheNeedingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "Project.Final.FeedingTheNeeding")
 public class FeedingTheNeedingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FeedingTheNeedingApplication.class, args);
