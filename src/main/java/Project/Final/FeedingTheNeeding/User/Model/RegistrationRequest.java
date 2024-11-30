33553f54d79b07604cc6aed8b3993796a16d510a
Eden Nahum <edennahu@post.bgu.ac.il>
2024-11-30 23:11:58 +0200

    adding components annotations

--- src/main/java/Project/Final/FeedingTheNeeding/User/Model/RegistrationRequest.java
+++ src/main/java/Project/Final/FeedingTheNeeding/User/Model/RegistrationRequest.java
@@ -1,10 +1,16 @@
 package Project.Final.FeedingTheNeeding.User.Model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Data
 @Builder
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegistrationRequest {
     //private String ID;
     private String email;
