6005ab3e80c144b69190a91fa18cb7754935885c
Eden Nahum <edennahu@post.bgu.ac.il>
2024-12-06 13:31:20 +0200

    test

--- src/main/java/Project/Final/FeedingTheNeeding/User/Service/UserService.java
+++ src/main/java/Project/Final/FeedingTheNeeding/User/Service/UserService.java
@@ -2,11 +2,15 @@ package Project.Final.FeedingTheNeeding.User.Service;
 
 import Project.Final.FeedingTheNeeding.User.Controller.UserController;
 import Project.Final.FeedingTheNeeding.User.Model.BaseUser;
+import Project.Final.FeedingTheNeeding.User.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
 
 }
