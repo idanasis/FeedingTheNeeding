45d71c387568db1f781c6731f1ce73d34f7a8635
Eden Nahum <edennahu@post.bgu.ac.il>
2024-12-06 13:21:44 +0200

    test

--- src/main/java/Project/Final/FeedingTheNeeding/User/Model/NeederContactDTO.java
+++
@@ -1,9 +0,0 @@
-package Project.Final.FeedingTheNeeding.User.Model;
-public class NeederContactDTO {
-    String id;
-    String email;
-    String firstName;
-    String lastName;
-    String phoneNumber;
-    String address;
-}
