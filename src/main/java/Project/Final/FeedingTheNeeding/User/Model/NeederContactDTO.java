29bb2b9423bf67f983e05290e457b551b5a15d0a
Eden Nahum <edennahu@post.bgu.ac.il>
2024-12-06 13:17:04 +0200

    fix test

---
+++ src/main/java/Project/Final/FeedingTheNeeding/User/Model/NeederContactDTO.java
@@ -0,0 +1,9 @@
+package Project.Final.FeedingTheNeeding.User.Model;
+public class NeederContactDTO {
+    String id;
+    String email;
+    String firstName;
+    String lastName;
+    String phoneNumber;
+    String address;
+}
---
+++ src/main/java/Project/Final/FeedingTheNeeding/User/Repository/JpaUserRepository.java
@@ -0,0 +1,16 @@
+package Project.Final.FeedingTheNeeding.User.Repository;
+import Project.Final.FeedingTheNeeding.User.Model.BaseUser;
+import Project.Final.FeedingTheNeeding.User.Model.UserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface JpaUserRepository extends JpaRepository<BaseUser, String>, IUserRepository {
+    //  @Override
+    // Optional<BaseUser> findByEmail(String email);
+    //  @Override
+    //  boolean existsByEmail(String email);
+//    @Override
+//    List<BaseUser> findByStatus(UserStatus status);
+}
