107b1a816f5168075ae5795e56f4b98cd1232431
ShavitMor <102467231+ShavitMor@users.noreply.github.com>
2024-12-12 18:21:07 +0200

    aboutToHome

--- front/src/Home/HomePage.jsx
+++ front/src/Home/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../Home/components/Header';
 import Introduce from '../Home/components/Introduce';
+import About from '../Home/components/About';
+import '../Home//styles/home.css';
 
 const HomePage = () => {
     return (
         <>
         <Header/>
         <Introduce/>
+        <About/>
         </>
     );
 };
