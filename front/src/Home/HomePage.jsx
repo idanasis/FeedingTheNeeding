e901fd3ac67ac8877ca3b0eb759ba0d56d2b6d22
ShavitMor <102467231+ShavitMor@users.noreply.github.com>
2024-12-15 18:54:33 +0200

    added pictures and fixed toggles

--- front/src/Home/HomePage.jsx
+++ front/src/Home/HomePage.jsx
@@ -3,6 +3,7 @@ import Header from '../Home/components/Header';
 import Introduce from '../Home/components/Introduce';
 import About from '../Home/components/About';
 import Projects from '../Home/components/Projects';
+import Pictures from '../Home/components/Pictures';
 import '../Home/styles/home.css';
 
 const HomePage = () => {
@@ -12,6 +13,7 @@ const HomePage = () => {
         <Introduce/>
         <About/>
         <Projects/>
+        <Pictures/>
         </>
     );
 };
