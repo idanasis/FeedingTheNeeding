232c0357bb79d16d5835cbd86c98fbcb294a3c77
Eden Nahum <edennahu@post.bgu.ac.il>
2024-12-22 15:39:22 +0200

    updates

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import HomePage from './Home/HomePage';
 import NeederTracking from './Social/components/NeederTracking';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DonorRegister from "./Authentication/Components/DonorRegister";
-import Login from "./Authentication/Components/Login";
 
 
 
 function App() {
-  return (
-  <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/social" element={<NeederTracking />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/donorRegister" element={<DonorRegister />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/social" element={<NeederTracking />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
