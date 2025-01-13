834677d2c51e1b01be49b7c630e9719434026e92
ShavitMor <102467231+ShavitMor@users.noreply.github.com>
2025-01-13 12:19:12 +0200

    added security for goPage

--- front/src/GoPage/GoPage.jsx
+++ front/src/GoPage/GoPage.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './DiveHeader';
 import GoManager from './GoManager';
 import GoUser from './GoUser';
+import {getUserRoleFromJWT} from './goPageAPI';
 
 const GoPage = () => {
-    return (
-        <>
-          <Header/>
-          <GoManager/>  
-            <GoUser/>
-   </>
-    );
+  const [role, setRole] = useState(null);
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      try {
+        const token = localStorage.getItem('token'); // Replace 'authToken' with the actual key used in localStorage.
+        if (!token) {
+          console.warn('No token found in localStorage.');
+          setRole(null);
+          return;
+        }
+
+        const userRole = await getUserRoleFromJWT(token);
+        setRole(userRole); 
+      } catch (error) {
+        console.error('Error fetching role:', error);
+        setRole(null); 
+      }
+    };
+
+    fetchRole();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      {(role === 'STAFF' || role=='ADMIN') && <GoManager />}
+      <GoUser />
+    </>
+  );
 };
 
 export default GoPage;
